a985746e2a670ea012c4e79aa48f006bcada5dc5
upasanasaha <upasanasaha40@gmail.com>
2023-04-26 00:42:02 +0530

    Data dumped.

---
+++ data_dump.py
@@ -0,0 +1,23 @@
+import pymongo
+import pandas as pd
+import json
+from predictor.config import mongo_client
+
+DATA_FILE_PATH="/config/workspace/insurance.csv"
+DATABASE_NAME="insurance"
+COLLECTION_NAME="details"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    #Dropping duplicate records
+    df.drop_duplicates(inplace=True)
+
+    #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+    
+    #insert converted json record to mongo db
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
