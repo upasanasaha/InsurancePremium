7fa30feba5deefda07b29cd7f0baca0ef613a427
upasanasaha <upasanasaha40@gmail.com>
2023-04-26 12:54:22 +0530

    Data ingestion completed.

--- predictor/components/data_ingestion.py
+++ predictor/components/data_ingestion.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+
 class DataIngestion:
 
     def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
