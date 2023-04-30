80e8bda500d9117d46efa4cbef30571b17e2aa4c
upasanasaha <upasanasaha40@gmail.com>
2023-04-30 17:25:01 +0530

    Batch pipeline completed.

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ file_path = "/config/workspace/insurance.csv"
 print(__name__)
 if __name__=="__main__":
     try:
-        #start tarining pipeline
-        start_training_pipeline()
+        #start training pipeline
+        # start_training_pipeline()
 
-        #start_training_pipeline()
-        # output_file=start_batch_prediction(input_file_path=file_path)
-        # print(output_file)
+        #start batch pipeline
+        output_file=start_batch_prediction(input_file_path=file_path)
+        print(output_file)
     except Exception as e:
         print(e)
