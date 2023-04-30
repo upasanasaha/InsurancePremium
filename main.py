fce6f768dd953a02e6067ca2c10cd1ecb15f0d77
upasanasaha <upasanasaha40@gmail.com>
2023-04-30 15:46:25 +0530

    Model evaluation completed.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import sys,os
 from predictor.entity import config_entity
 from predictor.components.data_ingestion import DataIngestion
 from predictor.components.data_validation import DataValidation
-# from predictor.components.data_transformation import DataTransformation
-# from predictor.components.model_trainer import ModelTrainer
-# from predictor.components.model_evaluation import ModelEvaluation
-# from predictor.components.model_pusher import ModelPusher
+from predictor.components.data_transformation import DataTransformation
+from predictor.components.model_trainer import ModelTrainer
+from predictor.components.model_evaluation import ModelEvaluation
+from predictor.components.model_pusher import ModelPusher
 
 if __name__=="__main__":
     try:
@@ -25,6 +25,31 @@ if __name__=="__main__":
         data_validation= DataValidation(data_validation_config=data_validation_config, 
                         data_ingestion_artifact=data_ingestion_artifact)
         data_validation_artifact = data_validation.initiate_data_validation()
+
+        #data transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+               data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact= data_transformation.initiate_data_transformation()
+
+        #model trainer
+        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, 
+               data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        #model evaluation
+        model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+        model_eval = ModelEvaluation(model_eval_config= model_eval_config,data_ingestion_artifact=data_ingestion_artifact,
+           data_transformation_artifact=data_transformation_artifact,model_trainer_artifact=model_trainer_artifact)
+        model_eval_artifact = model_eval.initiate_model_evaluation()
     
+        #model pusher
+        model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+        model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
+               data_transformation_artifact=data_transformation_artifact,
+               model_trainer_artifact=model_trainer_artifact)
+        model_pusher_artifact = model_pusher.initiate_model_pusher()
+
     except Exception as e:
         raise SensorException(e,sys)
